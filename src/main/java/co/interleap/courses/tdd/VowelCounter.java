03fb0b978ccbc40100a8a64f5934dd8fdb71985b
kadamp <piyush.kadam@cdk.com>
2019-11-09 19:34:56 +0530

    The first 3 test cases tested and development is done based on that test cases

--- src/main/java/co/interleap/courses/tdd/VowelCounter.java
+++ src/main/java/co/interleap/courses/tdd/VowelCounter.java
@@ -1,4 +1,22 @@
 package co.interleap.courses.tdd;
 
 public class VowelCounter {
+    public int count(String input){
+        int count = 0;
+        if(input.length() == 0){
+            return count;
+        }
+        else{
+            char []vowels = {'a','e','i','o','u'};
+            for(int index=0;index < input.length();index++){
+                char input_char = input.charAt(index);
+                for(char vowel:vowels){
+                    if(input_char == vowel){
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
 }
--- src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
+++ src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
@@ -2,8 +2,22 @@ package co.interleap.courses.tdd;
 
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
+
 public class VowelCounterTest {
 
     @Test
-    public void dummy(){}
+    public void shouldCountToZeroWithAnEmptyString(){
+        assertEquals(0, new VowelCounter().count(""));
+    }
+
+    @Test
+    public void shouldCountToOneWithAStringContainingOneVowel(){
+        assertEquals(1, new VowelCounter().count("e"));
+    }
+
+    @Test
+    public void shouldCountVowelsWithStringContainingMultipleVowels(){
+        assertEquals(10, new VowelCounter().count("aaeeiioouu"));
+    }
 }
